639e4f72670f8c690df05154cf4ad5cebc647c9e
Bitti09 <bitti06@gmail.com>
2016-09-04 13:03:16 +0200

    Test N+11

--- cancro.mk
+++ cancro.mk
@@ -99,11 +99,6 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
-
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/releasetools/device_check.sh:install/bin/device_check.sh
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
