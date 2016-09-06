2a82c4ac287647ac29cedcaad47aa130dcbd49d2
DrRamm <kotsareu@gmail.com>
2016-09-06 12:03:24 +0300

    fix 2: remove bliss flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,8 +193,8 @@ DONT_DEXPREOPT_PREBUILTS := true
 -include vendor/xiaomi/cancro/BoardConfigVendor.mk
 
 # Bliss flags
-BLISS_KRAIT := true
-BLISS_PIPE := true
-BLISS_GRAPHITE := true
-TARGET_TC_ROM := 5.x-HYPER-uber
-TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+#BLISS_KRAIT := true
+#BLISS_PIPE := true
+#BLISS_GRAPHITE := true
+#TARGET_TC_ROM := 5.x-HYPER-uber
+#TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
